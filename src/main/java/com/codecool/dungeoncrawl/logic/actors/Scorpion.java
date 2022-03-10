27f3d3a3e124d588c7865a10d82ee0405e700f00
Attila László Laczkó <laczkoattilalaszlo@gmail.com>
2022-03-10 09:49:11 +0100

    Add Scoprion and Ghost classes

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Scorpion.java
@@ -0,0 +1,20 @@
+package com.codecool.dungeoncrawl.logic.actors;
+
+import com.codecool.dungeoncrawl.logic.Cell;
+
+public class Scorpion extends Actor {
+
+    public Scorpion(Cell cell, int attackStrength) {
+        super(cell, attackStrength);
+    }
+
+    @Override
+    public String getTileName() {
+        return null;
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+
+    }
+}
