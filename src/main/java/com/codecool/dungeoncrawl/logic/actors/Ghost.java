4f4bbfe916ac46da9d8aad1d645ab4baffc568f4
galambati <galambosati98@gmail.com>
2022-03-12 23:37:56 +0100

    comment new ghost move

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Ghost.java
@@ -12,11 +12,13 @@ public class Ghost extends Enemy {
     @Override
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
-        if (nextCell.getType() == CellType.FLOOR) {
+        if (nextCell.getType() == CellType.FLOOR || nextCell.getType() == CellType.WALL) {
             cell.setActor(null);
-            cell.setType(CellType.FLOOR);
+            if (cell.getType() == CellType.WALL && nextCell.getType() == CellType.FLOOR){
+                cell.setType(CellType.WALL);
+            } else if (cell.getType() == CellType.FLOOR)
             nextCell.setActor(this);
-            nextCell.setType(CellType.PLAYER);
+            nextCell.setType(CellType.GHOST);
             cell = nextCell;
         } else if (nextCell.getType() == CellType.PLAYER && nextCell.getGameMap().getPlayer().getInventory2().containsKey("sword")) {
             nextCell.getGameMap().getPlayer().removeFromInventory2("sword");
@@ -30,4 +32,29 @@ public class Ghost extends Enemy {
         return "ghost";
     }
 
+//    @Override
+//    public void move(int dx, int dy) {
+//        Cell nextCell = cell.getNeighbor(dx, dy);
+//        System.out.println(cell.getType());
+//        System.out.println(nextCell.getType());
+//        if (nextCell.getType() == CellType.FLOOR || nextCell.getType() == CellType.WALL) {
+//            cell.setActor(null);
+//            if (cell.getType() == CellType.WALL && nextCell.getType() == CellType.FLOOR) {
+//                System.out.println("wall to floor");
+//                cell.setType(CellType.WALL);
+//            } else if (cell.getType() == CellType.FLOOR && nextCell.getType() == CellType.WALL) {
+//                System.out.println("floor to wall");
+//                cell.setType(CellType.FLOOR);
+//            } else {
+//                cell.setType(nextCell.getType());
+//                System.out.println("same");
+//            }
+//            nextCell.setActor(this);
+////            nextCell.setType(CellType.GHOST); eznemkell
+//            cell = nextCell;
+//        } else if (nextCell.getType() == CellType.PLAYER && nextCell.getGameMap().getPlayer().getInventory2().containsKey("sword")) {
+//            nextCell.getGameMap().getPlayer().removeFromInventory2("sword");
+//            nextCell.getGameMap().getPlayer().setAttackStrength(nextCell.getGameMap().getPlayer().getAttackStrength() - 2);
+//        }
+//    }
 }
